8ad597405ebb97f8c50555234dfeaba4a777e07e
Aleksandr Rabtsevich <57217014+stavkidisq@users.noreply.github.com>
2022-09-08 22:40:06 +0300

    Delete Orders.sql

--- Orders.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE Orders (
-    ProductId INT NOT NULL,
-    CategoryId INT NULL,
-    FOREIGN KEY (ProductId) REFERENCES Products (Id),
-    FOREIGN KEY (CategoryId) REFERENCES Categories (Id)
-);
